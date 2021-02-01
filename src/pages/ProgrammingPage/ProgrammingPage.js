96a2fd798383cb3147cfeda6d6d42ba27335058e
smilejakdu <ash982416@gmail.com>
2021-02-01 21:49:58 +0900

    programming_select

--- src/pages/ProgrammingPage/ProgrammingPage.js
+++ src/pages/ProgrammingPage/ProgrammingPage.js
@@ -1,15 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ProgrammingPageBody,
   ProgrammingPageTitle,
+  InputCustom,
+  InputBorder,
 } from "./ProgrammingPage.style";
+
 const Programming = () => {
+  const [check, setCheck] = useState(false);
+  const handleCheckBoxChange = (e) => {
+    setCheck(e);
+  };
+
+  const laguagesList = ["python", "javascript", "java", "php", "C", "C++"];
+  const frameworks = [
+    "spring",
+    "django",
+    "flask",
+    "express",
+    "react",
+    "vue",
+    "laravel",
+  ];
+
   return (
     <ProgrammingPageBody>
-      <ProgrammingPageTitle>
-        사용하는 언어 와 프레임워크를 선택해 주세요.
-      </ProgrammingPageTitle>
-      <div></div>
+      {localStorage.getItem("token") ? (
+        <div>
+          <ProgrammingPageTitle>
+            사용하는 언어 와 프레임워크를 선택해 주세요.
+          </ProgrammingPageTitle>
+
+          <InputBorder>
+            {laguagesList.map((laguages) => (
+              <div>
+                <InputCustom>
+                  <label>
+                    <input
+                      type="checkbox"
+                      onClick={(e) => {
+                        console.log(e.target.id);
+                      }}
+                    />
+                    <span className="customCheckBox" />
+                    <h4>{laguages}</h4>
+                  </label>
+                </InputCustom>
+              </div>
+            ))}
+          </InputBorder>
+          <InputBorder>
+            {frameworks.map((framework) => (
+              <div>
+                <InputCustom>
+                  <label>
+                    <input
+                      type="checkbox"
+                      onClick={(e) => {
+                        console.log(e.target.id);
+                      }}
+                    />
+                    <span className="customCheckBox" />
+                    <h4>{framework}</h4>
+                  </label>
+                </InputCustom>
+              </div>
+            ))}
+          </InputBorder>
+        </div>
+      ) : (
+        <ProgrammingPageTitle>로그인이 필요합니다.</ProgrammingPageTitle>
+      )}
     </ProgrammingPageBody>
   );
 };
--- src/pages/ProgrammingPage/ProgrammingPage.style.js
+++ src/pages/ProgrammingPage/ProgrammingPage.style.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import palette from "../../util/styles/palette";
 
 const ProgrammingPageBody = styled.div`
-  border: 1px solid red;
   margin-top: 10rem;
   margin-right: auto;
   margin-left: auto;
@@ -11,11 +10,60 @@ const ProgrammingPageBody = styled.div`
 `;
 
 const ProgrammingPageTitle = styled.div`
-  border: 1px solid blue;
   margin: 0 auto;
+  margin-bottom: 10rem;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-export { ProgrammingPageBody, ProgrammingPageTitle };
+const InputBorder = styled.div`
+  display: flex;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 40px;
+  margin-top: 20px;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 1100px) {
+    display: inline-block;
+  }
+`;
+
+const InputCustom = styled.div`
+  margin-right: 20px;
+  label {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    height: 13px;
+
+    input {
+      display: none;
+    }
+
+    span {
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      border: 1px solid ${palette.base_clor[2]};
+      border-radius: 3px;
+      background: white;
+    }
+
+    input:checked ~ span {
+      background: ${palette.base_clor[0]};
+    }
+
+    h4 {
+      margin-left: 10px;
+    }
+    @media (max-width: 1100px) {
+      margin-bottom: 20px;
+      margin-top: 20px;
+    }
+  }
+`;
+
+export { ProgrammingPageBody, ProgrammingPageTitle, InputCustom, InputBorder };
